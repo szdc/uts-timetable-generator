719e5503ac00201e86e587170df9c1ddf7ee8c78
szdc <jack.willis-craig@hotmail.com>
2015-02-16 23:29:31 +1100

    Changed the radio button group directive HTML to the AngularJS style.

--- app/directives/buttonsRadioDirective.js
+++ app/directives/buttonsRadioDirective.js
@@ -2,17 +2,12 @@ app.directive('buttonsRadio', function() {
   var directiveDefinitionObject = {
     restrict: 'E',
     scope: { model: '=', options:'='},
-    controller: function ($scope){
-      $scope.activate = function (option){
-        $scope.model = option;
-      };      
-    },
-    template: '<div class="btn-group" data-toggle="buttons">' +
+    template: '<div class="btn-group">' +
                 '<label class="btn btn-primary btn-sm" ' +
-                  'ng-class="{active: option === model}" ' +
                   'ng-repeat="option in options" ' +
-                  'ng-click="activate(option)">' +
-                    '<input type="radio">{{option}}' +
+                  'ng-model="$parent.model" ' +
+                  'btn-radio="{{option}}">' +
+                    '{{option}}' +
                 '</label>' +
               '</div>'
   };
